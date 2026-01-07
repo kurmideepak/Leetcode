bc0416377ca1faaa58fa134ab6f7f999daf6cced
Dipak Kurmi <kurmideepak7@gmail.com>
2026-01-08 01:08:20 +0530

    Leetcode 1339

---
+++ src/_1339_Maximum_product_of_Splitted_Binary_Tree.java
@@ -0,0 +1,22 @@
+public class _1339_Maximum_product_of_Splitted_Binary_Tree {
+    long totalSum = 0, maxProduct = 0;
+    static final int MOD = 1_000_000_007;
+    public int maxProduct(TreeNode root) {
+        totalSum = getTotalSum(root);
+        getSubtreeSum(root);
+        return (int)(maxProduct % MOD);
+    }
+    private long getTotalSum(TreeNode node){
+        if(node == null) return 0;
+        return node.val + getTotalSum(node.left) + getTotalSum(node.right);
+    }
+    private long getSubtreeSum(TreeNode node){
+        if(node == null) return 0;
+        long left = getSubtreeSum(node.left);
+        long right = getSubtreeSum(node.right);
+        long subTreeSum = left + right + node.val;
+        long product = subTreeSum * (totalSum - subTreeSum);
+        maxProduct = Math.max(maxProduct, product);
+        return subTreeSum;
+    }
+}
