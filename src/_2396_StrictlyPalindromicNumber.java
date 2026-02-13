eed7363717fe1ae9402a57f8b0a94c6ab5982460
Dipak Kurmi <kurmideepak7@gmail.com>
2026-02-13 22:42:04 +0530

    Leetcode 2396

---
+++ src/_2396_StrictlyPalindromicNumber.java
@@ -0,0 +1,9 @@
+public class _2396_StrictlyPalindromicNumber {
+
+    // Copy and Paste in Solution Class in Leetcode
+
+
+    public boolean isStrictlyPalindromic(int n) {
+        return false;
+    }
+}
