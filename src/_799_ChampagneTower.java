6b67028b430b70b6b3e23e4ab58fa4f8dfc6f4bd
Dipak Kurmi <kurmideepak7@gmail.com>
2026-02-14 16:16:10 +0530

    Leetcode 799

---
+++ src/_799_ChampagneTower.java
@@ -0,0 +1,37 @@
+public class _799_ChampagneTower {
+
+    // Copy and Paste in Solution Class in Leetcode
+
+
+    double[][] t = new double[101][101];
+    public double solve(int poured, int i, int j){
+        if(i < 0 || j < 0 || i < j){
+            return 0.0;
+        }
+        if(i == 0 && j == 0){
+            return poured;
+        }
+
+        if(t[i][j] != -1){
+            return t[i][j];
+        }
+        double left_up = (solve(poured, i-1, j-1) - 1)/2.0;
+        double right_up = (solve(poured, i-1, j) - 1)/2.0;
+
+        if(left_up < 0){
+            left_up = 0.0;
+        }
+        if(right_up < 0){
+            right_up = 0.0;
+        }
+        return t[i][j] = left_up + right_up;
+    }
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        for(int i = 0; i < 101; i++){
+            for(int j = 0; j < 101; j++){
+                t[i][j] = -1;
+            }
+        }
+        return Math.min(1.0, solve(poured, query_row, query_glass));
+    }
+}
