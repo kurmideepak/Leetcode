78cf4bc1a46d2013e0e5966e13ae508dfe036979
Dipak Kurmi <kurmideepak7@gmail.com>
2026-02-17 22:34:42 +0530

    Leetcode 401

---
+++ src/_401_BinaryWatch.java
@@ -0,0 +1,25 @@
+public class _401_BinaryWatch {
+
+    // Copy and Paste in Solution Class in Leetcode
+
+
+    public List<String> readBinaryWatch(int turnedOn) {
+
+        List<String> result = new ArrayList<>();
+
+        for (int hour = 0; hour < 12; hour++) {
+
+            for (int minute = 0; minute < 60; minute++) {
+
+                if (Integer.bitCount(hour) + Integer.bitCount(minute) == turnedOn) {
+
+                    String time = hour + ":" + (minute < 10 ? "0" + minute : minute);
+
+                    result.add(time);
+                }
+            }
+        }
+        return result;
+    }
+
+}
