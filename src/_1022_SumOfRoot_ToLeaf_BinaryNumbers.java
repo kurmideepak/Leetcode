1aa5701a2e0b256c248fe4803299528f31796961
Dipak Kurmi <kurmideepak7@gmail.com>
2026-02-24 11:57:40 +0530

    Leetcode 1022

---
+++ src/_1022_SumOfRoot_ToLeaf_BinaryNumbers.java
@@ -0,0 +1,22 @@
+public class _1022_SumOfRoot_ToLeaf_BinaryNumbers {
+
+
+    // Copy and Paste in Solution Class in Leetcode
+
+
+    public int sumRootToLeaf(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    private int dfs(TreeNode node, int current) {
+        if (node == null) return 0;
+
+        current = (current << 1) | node.val;
+
+        if (node.left == null && node.right == null) {
+            return current;
+        }
+
+        return dfs(node.left, current) + dfs(node.right, current);
+    }
+}
