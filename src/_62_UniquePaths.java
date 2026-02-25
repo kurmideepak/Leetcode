7c3ecdbaa056ca2cb379766e064d2d0abad77d4c
Dipak Kurmi <kurmideepak7@gmail.com>
2026-02-26 00:04:39 +0530

    Leetcode 62

---
+++ src/_62_UniquePaths.java
@@ -0,0 +1,15 @@
+public class _62_UniquePaths {
+
+    // Copy and Paste in Solution Class in Leetcode
+
+
+    int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++){
+            if(i == 0 || j == 0) dp[i][j] = 1;
+            else dp[i][j] = dp[i][j-1] + dp[i-1][j];
+        }
+    }
+        return dp[m-1][n-1];
+}
+}
