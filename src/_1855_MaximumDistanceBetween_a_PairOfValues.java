a57e608453a54b667f54042bbca470f8977ccf84
Dipak Kurmi <kurmideepak7@gmail.com>
2026-04-19 09:53:02 +0530

    Leetcode 1855

---
+++ src/_1855_MaximumDistanceBetween_a_PairOfValues.java
@@ -0,0 +1,27 @@
+public class _1855_MaximumDistanceBetween_a_PairOfValues {
+
+    // Copy and paste the below code inside your solution class
+
+
+    public int maxDistance(int[] nums1, int[] nums2) {
+        int i = 0, j = 0;
+        int ans = 0;
+
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] <= nums2[j]) {
+                ans = Math.max(ans, j - i);
+                j++;
+            } else {
+                i++;
+            }
+        }
+
+        return ans;
+    }
+
+
+
+
+
+
+}
